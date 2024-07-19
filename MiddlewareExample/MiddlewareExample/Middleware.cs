44e4f3698018ca5fc3ac443ca49ac436d606c431
vard14almaz <156775568+sonaalmaz@users.noreply.github.com>
2024-07-19 14:51:45 +0400

    Added the Identity entity package, used custom middleware in the Program.cs file, and separated it into a Middleware class, where we need to provide HttpContext as an argument for the invoking method.

---
+++ MiddlewareExample/MiddlewareExample/Middleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MiddlewareExample
+{
+    public class Middleware
+    {
+        private RequestDelegate _next;
+
+        public Middleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.ContentType = "application/json";
+            }
+            if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+            {
+                await context.Response.WriteAsync("Class based Middleware \n");
+            }
+
+            await _next(context);
+        }
+    }
+}
